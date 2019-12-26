2accf270cfdced4fc65d67e8f4442207f3a7cc96
J <mrjosemlrod@gmail.com>
2019-12-26 02:06:09 -0500

    modified .graphqlconfig to be able to get generated schema from prisma

--- src/prisma.js
+++ src/prisma.js
@@ -1,6 +1,6 @@
 import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
+    typeDefs: 'src/generated/rodriguez-predict-a-goal.graphql',
     endpoint: 'localhost:4466'
 })
