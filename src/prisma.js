36e7402d5d371a68e0cdbcd28e43b7393d14eb01
J <mrjosemlrod@gmail.com>
2020-01-13 10:30:38 -0500

    updated user schema to accept password

--- src/prisma.js
+++ src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
     typeDefs: 'src/generated/rodriguez-predict-a-goal.graphql',
-    endpoint: 'http://localhost:4466'
+    endpoint: 'http://localhost:4466',
+    secret: 'thisismysupersecrettext'
 })
 
 export default prisma;
